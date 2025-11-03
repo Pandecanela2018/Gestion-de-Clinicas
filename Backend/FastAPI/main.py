862d964142e9011e7ba15c9658a33e4726af5d01
Francisco <franciscojgt44@gmail.com>
2025-11-03 14:34:28 -0600

    Api paciente terminada

--- Backend/FastAPI/main.py
+++ Backend/FastAPI/main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
-from routers import doctor
+from routers import doctor,patient
 
 app = FastAPI()
 
 app.include_router(doctor.router)
+
+app.include_router(patient.router)
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
