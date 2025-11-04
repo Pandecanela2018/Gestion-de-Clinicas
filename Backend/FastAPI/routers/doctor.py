9206b178baeddc9301703f85bda3d9a420ed4a44
jaime <jaimelicinio550@gmail.com>
2025-11-03 21:21:51 -0600

    Se implemento la api para horario y citas

--- Backend/FastAPI/routers/doctor.py
+++ Backend/FastAPI/routers/doctor.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, HTTPException, status
-from db.models.doctor import Doctor
-from db.schemas.doctor import doctor_schema, doctors_schema
+from db.models.Doctor import Doctor
+from db.schemas.Doctor import doctor_schema, doctors_schema
 from db.client import db_client
 from bson import ObjectId
 
